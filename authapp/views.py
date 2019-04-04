c6c5a0dad1e1b81c90cc46a9f251cc2d0dc6d623
Jo <geekbrains@gb.local>
2019-04-04 22:31:50 +0500

    lesson 6:     - basket methods     - hot_product     - basket page     - product page     - @login_required

--- authapp/views.py
+++ authapp/views.py
@@ -5,22 +5,26 @@ from django.urls import reverse
 
 
 def login(request):
+    next = request.GET['next'] if 'next' in request.GET.keys() else ''
     if request.method == 'POST':
         form = ShopUserLoginForm(data=request.POST)
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
-
             user = auth.authenticate(username=username, password=password)
             if user and user.is_active:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('main:index'))
+                if 'next' in request.POST.keys():
+                    return HttpResponseRedirect(request.POST['next'])
+                else:
+                    return HttpResponseRedirect(reverse('main:index'))
     else:
         form = ShopUserLoginForm()
 
     context = {
         'title': 'вход в систему',
-        'form': form
+        'form': form,
+        'next': next
     }
     return render(request, 'authapp/login.html', context)
 
--- basketapp/views.py
+++ basketapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from mainapp.models import Product
 from basketapp.models import Basket
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 
+@login_required
 def index(request):
     basket_items = request.user.basket.order_by('product__category')
 
@@ -13,7 +16,14 @@ def index(request):
     return render(request, 'basketapp/index.html', context)
 
 
+@login_required
 def add(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('main:product',
+                                            kwargs={
+                                                'pk': pk,
+                                            }))
+
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
 
@@ -26,6 +36,7 @@ def add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def remove(request, pk):
     get_object_or_404(Basket, pk=pk).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
