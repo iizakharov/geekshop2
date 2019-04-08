8aaf3400076e301f53c15081ca62558201d72d48
Jo <geekbrains@gb.local>
2019-04-08 21:37:40 +0500

    lesson 7:     - adminapp     - index page

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from authapp.models import ShopUser
+
+
 def index(request):
-    pass
+    users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser',
+                                                 '-is_staff', 'username')
+    context = {
+        'title': 'админка/пользователи',
+        'objects': users_list
+    }
+
+    return render(request, 'adminapp/index.html', context)
