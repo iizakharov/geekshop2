c1d729ec3b22052ebb0c46dbe2a2f88fcedd3589
Jo <geekbrains@gb.local>
2019-04-08 22:44:11 +0500

    lesson 7:     - adminapp     - index page     - categories page     - category products     - link to admin     - user C_UD

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     re_path(r'^$', adminapp.index, name='index'),
     re_path(r'^categories/$', adminapp.categories, name='categories'),
     re_path(r'^products/(?P<pk>\d+)/$', adminapp.products, name='products'),
+
+    re_path(r'^shopuser/create/$', adminapp.shopuser_create, name='shopuser_create'),
+    re_path(r'^shopuser/update/(?P<pk>\d+)/$', adminapp.shopuser_update, name='shopuser_update'),
+    re_path(r'^shopuser/delete/(?P<pk>\d+)/$', adminapp.shopuser_delete, name='shopuser_delete'),
 ]
