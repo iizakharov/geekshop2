96ec55330b8128cde187d1449e4c5f276ecbfc15
Jo <carwash067@gmail.com>
2019-04-30 23:04:16 +0500

    lesson 2.2     - google auth     - update_db     - ShopUserProfile     - ShopUserProfile update     - pipeline

---
+++ authapp/pipeline.py
@@ -0,0 +1,30 @@
+from social_core.exceptions import AuthForbidden
+
+from authapp.models import ShopUserProfile
+
+
+def save_user_profile(backend, user, response, *args, **kwargs):
+    if backend.name == "google-oauth2":
+        print(response.keys())
+        if 'gender' in response.keys():
+            if response['gender'] == 'male':
+                user.shopuserprofile.gender = ShopUserProfile.MALE
+            else:
+                user.shopuserprofile.gender = ShopUserProfile.FEMALE
+
+        if 'tagline' in response.keys():
+            user.shopuserprofile.tagline = response['tagline']
+
+        if 'aboutMe' in response.keys():
+            user.shopuserprofile.aboutMe = response['aboutMe']
+
+        if 'ageRange' in response.keys():
+            minAge = response['ageRange']['min']
+            if int(minAge) < 18:
+                user.delete()
+                raise AuthForbidden('social_core.backends.google.GoogleOAuth2')
+        user.save()
+    elif backend.name == "vk-oauth2":
+        pass
+
+    return
