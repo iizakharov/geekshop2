f9cfa6c0093a6607c5a7b88c69906f4edd1c59b6
Jo <carwash067@gmail.com>
2019-04-30 22:24:03 +0500

    lesson 2.2     - google auth     - update_db

--- authapp/views.py
+++ authapp/views.py
@@ -93,7 +93,7 @@ def verify(request, email, activation_key):
         if user.activation_key == activation_key and user.is_activation_key_valid():
             user.is_active = True
             user.save()
-            auth.login(request, user)
+            auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
         else:
             print(f'error activation user: {user}')
         return render(request, 'authapp/verification.html')
---
+++ mainapp/management/commands/update_db.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from mainapp.models import ProductCategory, Product
+from authapp.models import ShopUser, ShopUserProfile
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        to_update = ShopUser.objects.filter(shopuserprofile__isnull=True)
+        print(len(to_update))
+        for user in to_update:
+            ShopUserProfile.objects.create(user=user)
+
+
