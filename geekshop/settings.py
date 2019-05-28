ed742fbf1b7bca539478141d5e8869523f5db25f
Jo <jo@gb.local>
2019-05-28 22:47:29 +0500

    lesson 2.8     - conditional expressions     - first test

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')v7v(@&r&hix3nr$1!b3%sid*yx3pbt68jk+zk9#f37ag0^jjg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
